0ea6c4f8872893a874d793195ee15420885f7f3d
Boldchingis <your-email@example.com>
2024-12-24 17:39:43 +0800

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
 import Image from "next/image";
 
-export default function Home() {
+export const API_KEY = "f39690f9830ce804b7894ac1def4f7e9";
+
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMzk2OTBmOTgzMGNlODA0Yjc4OTRhYzFkZWY0ZjdlOSIsIm5iZiI6MTczNDk0OTM3MS43NDIsInN1YiI6IjY3NjkzOWZiYzdmMTcyMDVkMTBiMGIxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2r2TerxSJdZGmGVSLVDkk6nHT0NPqY4rOcxHtMNt0aE",
+  },
+};
+type Movie = {
+  title: string;
+  rating: number;
+};
+export default async function Home() {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1`,
+    options
+  );
+  const data = await res.json();
+  const movies: Movie[] = data.results;
   return (
     <div>
-      <div className="bg-[grey] w-[500px] h-[500px] flex justify-center items-center rounded-2xl"></div>
+      {movies.map((movie) => (
+        <div>{movie.title}</div>
+      ))}
     </div>
   );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,45 @@
+import { Film } from "lucide-react";
+import { Mail } from "lucide-react";
+import { Phone } from "lucide-react";
+
+export function Footer() {
+  return (
+    <div className="bg-[#4338CA] text-white w-full py-8">
+      <div className="flex flex-col mb-8 ml-8">
+        <div className="flex">
+          <Film className="text-white mr-2" />
+          <p className="text-2xl font-bold">Movie Z</p>
+        </div>
+        <p className="mb-4">© 2024 Movie Z. All Rights Reserved</p>
+      </div>
+      <section className="flex flex-wrap justify-around gap-12 px-8">
+        <div className="flex flex-col items-start text-sm space-y-4">
+          <p className="font-semibold">Contact Information</p>
+          <div className="flex items-center space-x-2">
+            <Mail className="text-white" />
+            <div>
+              <p>Email:</p>
+              <p>support@movieZ.com</p>
+            </div>
+          </div>
+          <div className="flex items-center space-x-2">
+            <Phone className="text-white" />
+            <div>
+              <p>Phone:</p>
+              <p>+976 (11) 123-4567</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Social Media */}
+        <div className="flex flex-col items-start text-sm space-y-2">
+          <p className="font-semibold">Follow us</p>
+          <p>Facebook</p>
+          <p>Instagram</p>
+          <p>Twitter</p>
+          <p>Youtube</p>
+        </div>
+      </section>
+    </div>
+  );
+}
